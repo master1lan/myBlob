b845ab21584f2786ccbafa0a4736e7c09a55936c
master1lan <278457198@qq.com>
2022-06-19 20:59:17 +0800

    测试

--- server/config/config.default.js
+++ server/config/config.default.js
@@ -1,5 +1,8 @@
 /* eslint valid-jsdoc: "off" */
 
+//配置文件
+
+const env=require("./env");
 'use strict';
 
 /**
@@ -20,44 +23,8 @@ module.exports = appInfo => {
   config.jwt = {
     secret: 'Nick',
   };
-  config.mysql = {
-    // 单数据库信息配置
-    client: {
-      // host
-      host: 'localhost',
-      // 端口号
-      port: '3306',
-      // 用户名
-      user: 'root',
-      // 密码
-      password: 'suxiaobai2014',
-      // 数据库名
-      database: 'blob',
-      charset: 'utf8mb4',
-    },
-    // 是否加载到 app 上，默认开启
-    app: true,
-    // 是否加载到 agent 上，默认关闭
-    agent: false,
-  };
-  config.redis = {
-    clients: {
-      //存放token的
-      token: {
-        port: 6379,
-        host: '127.0.0.1',
-        password: "suxiaobai",
-        db: 0
-      },
-      //存放用户收藏夹的
-      lists:{
-        port:6379,
-        host: '127.0.0.1',
-        password: "suxiaobai",
-        db:1
-      }
-    }
-  };
+  config.mysql = env.mysql;
+  config.redis = env.redis;
   config.security = {
     csrf: {
       enable: false,
