3f6e1d85939b19924721eca58c7634ae6d656c64
master1lan <278457198@qq.com>
2022-06-20 17:42:59 +0800

    修复css

--- src/utils/message/index.js
+++ src/utils/message/index.js
@@ -23,7 +23,7 @@ const svgType = (() => {
             'M17.388,4.751H2.613c-0.213,0-0.389,0.175-0.389,0.389v9.72c0,0.216,0.175,0.389,0.389,0.389h14.775c0.214,0,0.389-0.173,0.389-0.389v-9.72C17.776,4.926,17.602,4.751,17.388,4.751 M16.448,5.53L10,11.984L3.552,5.53H16.448zM3.002,6.081l3.921,3.925l-3.921,3.925V6.081z M3.56,14.471l3.914-3.916l2.253,2.253c0.153,0.153,0.395,0.153,0.548,0l2.253-2.253l3.913,3.916H3.56z M16.999,13.931l-3.921-3.925l3.921-3.925V13.931z'
         ]
     ])
-    return (type)=> map.get(type)||map.get('info');
+    return (type) => map.get(type) || map.get('info');
 })();
 
 //单个message 
@@ -36,9 +36,11 @@ const Message = ({ msg, type, onClose }) => {
     }, []);
     return (
         <div className={styles.message}>
-            <svg className={styles.svgIcon} viewBox="0 0 20 20">
-                <path fill="none" d={svgType(type)}></path>
-            </svg>
+            <div>
+                <svg className={styles.svgIcon} viewBox='20 20' >
+                    <path fill="none" d={svgType(type)}></path>
+                </svg>
+            </div>
             <p><span>{msg}</span></p>
         </div>
     );
